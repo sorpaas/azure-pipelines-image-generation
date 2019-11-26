466155b17b8f5138b7dd95096136a4451bc191ed
Ivan Nosar <v-ivnosa@microsoft.com>
2019-11-26 13:25:15 +0300

    Install npm packages

--- images/linux/scripts/installers/1804/hosted-tool-cache.sh
+++ images/linux/scripts/installers/1804/hosted-tool-cache.sh
@@ -18,6 +18,9 @@ azcopy --recursive \
        --source https://vstsagenttools.blob.core.windows.net/tools/hostedtoolcache/ubuntu-1804 \
        --destination $AGENT_TOOLSDIRECTORY
 
+# Remove PyPy toolcache folder manually because azcopy doesn't support exclude flag
+rm -rf $AGENT_TOOLSDIRECTORY/PyPy/*
+
 # Install tools from hosted tool cache
 original_directory=$PWD
 setups=$(find $AGENT_TOOLSDIRECTORY -name setup.sh)
@@ -28,10 +31,26 @@ for setup in $setups; do
 	cd $original_directory;
 done;
 
+chmod -R 777 $AGENT_TOOLSDIRECTORY
+
+echo "Installing npm-toolcache..."
+PYPY_VERSIONS=( '2.7' '3.6' )
+
+for PYPY_VERSION in ${PYPY_VERSIONS[@]}; do
+    echo "Install PyPy-$PYPY_VERSION"
+    npm install toolcache-pypy-ubuntu-1804-x64@$PYPY_VERSION --registry=https://buildcanary.pkgs.visualstudio.com/PipelineCanary/_packaging/hostedtoolcache/npm/registry/
+done;
+
 DocumentInstalledItem "Python (available through the [Use Python Version](https://go.microsoft.com/fwlink/?linkid=871498) task)"
 pythons=$(ls $AGENT_TOOLSDIRECTORY/Python)
 for python in $pythons; do
 	DocumentInstalledItemIndent "Python $python"
+done;
+
+DocumentInstalledItem "PyPy (available through the [Use Python Version](https://go.microsoft.com/fwlink/?linkid=871498) task)"
+pypys=$(ls $AGENT_TOOLSDIRECTORY/PyPy)
+for pypy in $pypys; do
+	DocumentInstalledItemIndent "PyPy $pypy"
 done;
 
 DocumentInstalledItem "Ruby (available through the [Use Ruby Version](https://go.microsoft.com/fwlink/?linkid=2005989) task)"
--- images/win/scripts/Installers/Download-ToolCache.ps1
+++ images/win/scripts/Installers/Download-ToolCache.ps1
@@ -4,48 +4,74 @@
 ##  Desc:  Download tool cache
 ################################################################################
 
-Function InstallTool
-{
-    Param
-    (
-        [System.Object]$ExecutablePath
+Function Install-NpmPackage {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]
+        [System.String[]]
+        $Name,
+        [System.String]
+        $NpmRegistry="https://buildcanary.pkgs.visualstudio.com/PipelineCanary/_packaging/hostedtoolcache/npm/registry/"
     )
 
-    Write-Host $ExecutablePath.DirectoryName
-    Set-Location -Path $ExecutablePath.DirectoryName
-    Get-Location | Write-Host
-    if (Test-Path 'tool.zip')
-    {
+    foreach($packageName in $Name) {
+        Write-Host "Installing npm '$packageName' package from '$NpmRegistry'"
+        npm install $packageName --registry=$NpmRegistry
+    }
+}
+
+Function InstallTool {
+    [CmdletBinding()]
+    param(
+        [System.IO.FileInfo]$ExecutablePath
+    )
+
+    Set-Location -Path $ExecutablePath.DirectoryName -PassThru | Write-Host
+    if (Test-Path 'tool.zip') {
         Expand-Archive 'tool.zip' -DestinationPath '.'
     }
     cmd.exe /c 'install_to_tools_cache.bat'
 }
 
+# ToolCache Blob
 $SourceUrl = "https://vstsagenttools.blob.core.windows.net/tools"
 
+# HostedToolCache Path
 $Dest = "C:/"
 
 $Path = "hostedtoolcache/windows"
+$ToolsDirectory = $Dest + $Path
 
+# Add AzCopy to the Path
 $env:Path = "C:\Program Files (x86)\Microsoft SDKs\Azure\AzCopy;" + $env:Path
 
 Write-Host "Started AzCopy from $SourceUrl to $Dest"
+AzCopy /Source:$SourceUrl /Dest:$Dest /S /V /Pattern:$Path
 
-AzCopy /Source:$SourceUrl /Dest:$Dest  /S /V /Pattern:$Path
+# Temporary remove PyPy
+Remove-Item -Path C:\hostedtoolcache\windows\PyPy -Force -Recurse
 
-$ToolsDirectory = $Dest + $Path
-
-$current = Get-Location
-Set-Location -Path $ToolsDirectory
+# Install ToolCache
+Push-Location -Path $ToolsDirectory
 
 Get-ChildItem -Recurse -Depth 4 -Filter install_to_tools_cache.bat | ForEach-Object {
     #In order to work correctly Python 3.4 x86 must be installed after x64, this is achieved by current toolcache catalog structure
-    InstallTool($_)
+    InstallTool -ExecutablePath $_
 }
 
-Set-Location -Path $current
+Pop-Location
 
+# Define AGENT_TOOLSDIRECTORY environment variable
+$env:AGENT_TOOLSDIRECTORY = $ToolsDirectory
 setx AGENT_TOOLSDIRECTORY $ToolsDirectory /M
+
+# Install Python ToolCache
+$PyPyVersionsToolcacheInstall = @(
+    "toolcache-pypy-windows-x86@2.7"
+    "toolcache-pypy-windows-x86@3.6"
+)
+
+Install-NpmPackage -Name $PyPyVersionsToolcacheInstall
 
 #junction point from the previous Python2 directory to the toolcache Python2
 $python2Dir = (Get-Item -Path ($ToolsDirectory + '/Python/2.7*/x64')).FullName
